67fda092606f9ac2de3880cd2677980ea56615d0
nvtngan <nvtngan@hcmus.edu.vn>
2023-11-25 02:12:42 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    reactStrictMode: true,
+
+    // Add basePath
+    basePath: '/lug2024',
 }
 
 module.exports = nextConfig
